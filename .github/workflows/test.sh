c5e311e26b4ee0e47ab59b58e68e16fbaa28522a
Kevin Porras <kporras07@gmail.com>
2021-10-04 18:27:31 -0600

    Move tests to bash file.

---
+++ .github/workflows/test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -ex
+
+echo "Get wonder current version"
+./wonder info | grep "1.2"
+
+echo "Backup current binary"
+cp ./wonder ./wonder-backup
+
+echo "Self update --stable"
+cp -r ./wonder-backup wonder && ./wonder self:update --stable
+./wonder info | grep "2.0"
+
+echo "Self update"
+cp -r ./wonder-backup wonder && ./wonder self:update
+./wonder info | grep "2.0"
+
+echo "Self update --preview"
+cp -r ./wonder-backup wonder && ./wonder self:update --preview
+./wonder info | grep "2.1-beta1"
+
+echo "Self update --stable --compatible"
+cp -r ./wonder-backup wonder && ./wonder self:update --stable --compatible
+./wonder info | grep "1.3"
+
+echo "Self update --compatible"
+cp -r ./wonder-backup wonder && ./wonder self:update --compatible
+./wonder info | grep "1.3"
